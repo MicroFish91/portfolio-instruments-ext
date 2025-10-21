adc60c2820572ef02ce69629094e447df8c8b87c
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-10-20 21:21:54 -0700

    Add average weighted duration and rate display

--- src/tree/snapshots/snapshot/dashboard/maturationDate/SnapshotByMaturationDateItem.ts
+++ src/tree/snapshots/snapshot/dashboard/maturationDate/SnapshotByMaturationDateItem.ts
@@ -11,6 +11,7 @@ import { SnapshotMaturationStartItem } from "./SnapshotMaturationStartItem";
 import { SnapshotMaturationEndItem } from "./SnapshotMaturationEndItem";
 import { MaturationDateItem } from "./MaturationDateItem";
 import { SnapshotMaturationFilterItem } from "./SnapshotMaturationFilterItem";
+import { GenericItem } from "../../../../GenericItem";
 
 export class SnapshotByMaturationDateItem extends TreeItem implements PiExtTreeItem {
     static readonly contextValue: string = 'snapshotByMaturationDateItem';
@@ -49,12 +50,64 @@ export class SnapshotByMaturationDateItem extends TreeItem implements PiExtTreeI
         };
 
         const resources: MaturationDateResource[] = await SnapshotByMaturationDateItem.getSnapshotByMaturationDate(this.email, this.snapshotData.snap_id, options) ?? [];
+        const { durationYrs, interestRatePct } = this.computeWeightedAverageDurationAndInterestRate(resources);
+
         return [
             new SnapshotMaturationFilterItem(this, this.email, this.snapshotData),
             ...resources
                 .sort((a, b) => new Date(b.maturation_date).getTime() - new Date(a.maturation_date).getTime())
                 .map(resource => new MaturationDateItem(this, this.email, this.snapshotData, resource)),
+            new GenericItem({
+                id: `${this.id}/averageRate`,
+                label: interestRatePct + '%',
+                description: 'Average Weighted Rate',
+                contextValue: 'averageRateItem',
+                collapsibleState: TreeItemCollapsibleState.None,
+                iconPath: new ThemeIcon("graph-line", "white"),
+            }),
+            new GenericItem({
+                id: `${this.id}/averageDuration`,
+                label: durationYrs.toString() + ' Years',
+                description: 'Average Weighted Duration',
+                contextValue: 'averageDurationItem',
+                collapsibleState: TreeItemCollapsibleState.None,
+                iconPath: new ThemeIcon("graph-line", "white"),
+            }),
         ];
+    }
+
+    private computeWeightedAverageDurationAndInterestRate(resources: MaturationDateResource[]): { durationYrs: number; interestRatePct: number } {
+        const total: number = resources.reduce((sum, resource) => sum + resource.total, 0);
+
+        const weightedDurationsYrs: number[] = [];
+        const weightedInterestRatePcts: number[] = [];
+
+        for (const r of resources) {
+            const weight: number = r.total / total;
+
+            const weightedInterestRatePct: number = weight * r.interest_rate_pct;
+            weightedInterestRatePcts.push(weightedInterestRatePct);
+
+            const durationYears: number = this.calculateDurationInYears(r.maturation_date);
+            const weightedDuration: number = weight * durationYears;
+            weightedDurationsYrs.push(weightedDuration);
+        }
+
+        const weightedDuration: number = weightedDurationsYrs.reduce((sum, wd) => sum + wd, 0);
+        const weightedInterestRates: number = weightedInterestRatePcts.reduce((sum, wir) => sum + wir, 0);
+
+        return {
+            durationYrs: Math.round(weightedDuration * 100) / 100,
+            interestRatePct: Math.round(weightedInterestRates * 100) / 100,
+        };
+    }
+
+    private calculateDurationInYears(maturationDate: string): number {
+        const currentDate = new Date();
+        const matDate = new Date(maturationDate);
+        const timeDifferenceMs = matDate.getTime() - currentDate.getTime();
+        const durationYears = timeDifferenceMs / (1000 * 60 * 60 * 24 * 365.25); // 365.25 accounts for leap years
+        return Math.max(0, durationYears); // Ensure non-negative duration
     }
 
     static async getSnapshotByMaturationDate(email: string, snapId: number, options?: GetSnapshotMaturationDateOptions): Promise<MaturationDateResource[] | undefined> {
